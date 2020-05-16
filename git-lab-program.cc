4f31664386307b927f5545c2994994a4036cf12b
Paola <pg748418@ohio.edu>
2020-05-15 23:31:56 -0400

    Enter a message here

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Paola Garcia Morales
+ *        Date: May 15, 2020
+ * Description: Practice with Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
